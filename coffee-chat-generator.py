ce12c6ec39abb8538d34ad921caa8f247989da01
bokology <45399132+bokology@users.noreply.github.com>
2024-06-17 14:19:43 +0100

    Update coffee-chat-generator.py

--- coffee-chat-generator.py
+++ coffee-chat-generator.py
@@ -1,7 +1,34 @@
+import random
+
+# Define dictionary for team members
+team_members = {}
+
+# Define dictionary for topics/questions
+
+topic_questions = {}
 
 def main():
+    # Display a text menu and get user selection
     selected_option = display_menu()
-    print(selected_option)
+    
+    if selected_option == 1:
+        print(selected_option)
+    
+    elif selected_option == 2:
+        pass
+
+    elif selected_option == 3:
+        pass
+
+    elif selected_option == 4:
+        pass
+
+    elif selected_option ==5:
+        pass
+
+    elif selected_option ==6:
+        quit()
+
     print("\n")
 
 def display_menu():
@@ -10,8 +37,9 @@ def display_menu():
     print("1. Generate New Pairings")
     print("2. Add Members")
     print("3. Remove Members")
-    print("4. Add Topic/Question")
+    print("4. Add Topic Questions")
     print("5. Remove Topic Question")
+    print("6. Quit Application")
     print("\n")
 
     while True:
